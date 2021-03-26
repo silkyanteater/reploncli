493d58bfa5c11da2eae84d226ab00378cea79452
silkyanteater <cyclopesrufus@gmail.com>
2021-03-26 16:04:44 +0000

    dynamic prompt

--- reploncli/reploncli.py
+++ reploncli/reploncli.py
@@ -7,27 +7,36 @@ from cmd import Cmd
 
 class Repl(Cmd):
 
-    def __init__(self, cli_function, help=None):
+    def __init__(self, cli_function, prompt="", help=None):
         super().__init__()
         self.cli_function = cli_function
         self.help = help
+        self.prompt = str(prompt)
+        self._prompt = prompt
+        self._update_prompt()
+
+    def _update_prompt(self):
+        if callable(self._prompt):
+            self.prompt = str(self._prompt())
 
     def do_help(self, inp):
         if isinstance(self.help, str):
             print(self.help)
         elif callable(self.help):
             self.help()
+        self._update_prompt()
 
     def do_EOF(self, inp):
         print('^D')
         return True
 
     def emptyline(self):
-        pass
+        self._update_prompt()
 
     def default(self, inp):
         if inp.startswith('.') and len(inp) > 1:
             os.system(inp[1:])
+            self._update_prompt()
             return False
         try:
             self.cli_function(shlex.split(inp))
@@ -40,17 +49,15 @@ class Repl(Cmd):
             return True
         except:
             traceback.print_exc()
+        self._update_prompt()
         return False
 
 
 def reploncli(cli_function, repl_mode=None, help=None, prompt=""):
     if not callable(cli_function):
         raise TypeError("cli_function: callable expected")
-    if not isinstance(prompt, str):
-        raise TypeError("prompt: string expected")
     if repl_mode is True:
-        repl = Repl(cli_function, help)
-        repl.prompt = prompt
+        repl = Repl(cli_function, prompt, help)
         nothing_worse_than_keyboardinterrupt = True
         while(nothing_worse_than_keyboardinterrupt):
             nothing_worse_than_keyboardinterrupt = False
